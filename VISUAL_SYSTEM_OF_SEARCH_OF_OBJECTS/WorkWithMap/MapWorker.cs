f88e08aa0a148f140aec40b489b4106325781db0
Spautov <Pautovs@mail.ru>
2019-01-11 21:06:44 +0300

    Add files via upload

---
+++ VISUAL_SYSTEM_OF_SEARCH_OF_OBJECTS/WorkWithMap/MapWorker.cs
@@ -0,0 +1,145 @@
+﻿using CurrencyExchangeDAL;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithMap
+{
+    public class MapWorker
+    {
+        [Flags]
+        public enum SettingsShow
+        {
+            None = 0,
+            Name = 1,
+            BankName = 2,
+            Currency = 4,
+            TimeWork = 8,
+            Phone = 16,
+            Adress = 32,
+            Servises = 64
+        }
+
+        private GMapControl _gMapCtrl;
+        private GMapOverlay _markersOverlay;
+        public MapWorker(GMapControl MapCtrl)
+        {
+            _gMapCtrl = MapCtrl;
+            //Создаем новый список маркеров, с указанием компонента 
+            //в котором они будут использоваться и названием списка
+            _markersOverlay = new GMapOverlay();
+            //Добавляем в компонент, список маркеров 
+            _gMapCtrl.Overlays.Add(_markersOverlay);
+        }
+
+        private string BuildToolTripText(PointCurrencyExchange PointCurEx, SettingsShow settings)
+        {
+            string outstr = "";
+            if (settings.HasFlag(SettingsShow.Name))
+            {
+                if (!String.IsNullOrEmpty(PointCurEx.Name) && !String.IsNullOrWhiteSpace(PointCurEx.Name))
+                {
+                    outstr += PointCurEx.Name + Environment.NewLine;
+                }
+            }
+            if (settings.HasFlag(SettingsShow.BankName))
+            {
+                if (PointCurEx.Bank != null &&
+                    !String.IsNullOrEmpty(PointCurEx.Bank.Name) &&
+                    !String.IsNullOrWhiteSpace(PointCurEx.Bank.Name))
+                {
+                    outstr += PointCurEx.Bank.Name + Environment.NewLine;
+                }
+            }
+            if (settings.HasFlag(SettingsShow.Currency))
+            {
+                if (PointCurEx.Bank != null && PointCurEx.Bank.CurrencyExchangeInfo != null)
+                {
+                    if (PointCurEx.Bank.CurrencyExchangeInfo.Date != null && PointCurEx.Bank.CurrencyExchangeInfo.Currencys.Count > 0)
+                    {
+                        DateTime date = PointCurEx.Bank.CurrencyExchangeInfo.Date;
+                        outstr += String.Format("Курсы валют с {0} :", date.ToString()) + Environment.NewLine;
+
+                        foreach (var item in PointCurEx.Bank.CurrencyExchangeInfo.Currencys)
+                        {
+                            outstr += String.Format("{0}: покупка - {1:N4}  продажа - {2:N4}", item.Name, item.Sell, item.Buy) + Environment.NewLine;
+                        }
+                    }
+                }
+
+            }
+            if (settings.HasFlag(SettingsShow.TimeWork))
+            {
+                if (!String.IsNullOrEmpty(PointCurEx.TimeOfWork) && !String.IsNullOrWhiteSpace(PointCurEx.TimeOfWork))
+                {
+                    outstr += "Режим работы: " + PointCurEx.TimeOfWork + Environment.NewLine;
+                }
+            }
+            if (settings.HasFlag(SettingsShow.Phone))
+            {
+                if (!String.IsNullOrEmpty(PointCurEx.TelephonNumber) && !String.IsNullOrWhiteSpace(PointCurEx.TelephonNumber))
+                {
+                    outstr += "Телефон: " + PointCurEx.TelephonNumber + Environment.NewLine;
+                }
+            }
+            if (settings.HasFlag(SettingsShow.Adress))
+            {
+                if (!String.IsNullOrEmpty(PointCurEx.Address) && !String.IsNullOrWhiteSpace(PointCurEx.Address))
+                {
+                    outstr += "Адрес: " + PointCurEx.Address + Environment.NewLine;
+                }
+            }
+            if (settings.HasFlag(SettingsShow.Servises))
+            {
+                if (PointCurEx.ServicesType != null && PointCurEx.ServicesType.Count > 0)
+                {
+                    outstr += "Виды услуг: " + Environment.NewLine;
+                    foreach (var item in PointCurEx.ServicesType)
+                    {
+                        outstr += "  " + item.Name + Environment.NewLine;
+                    }
+                }
+
+            }
+            return outstr;
+        }
+
+        private void Clear()
+        {
+            _markersOverlay.Markers.Clear();
+        }
+        private void ShowCurrencyExchange(PointCurrencyExchange PointCurEx, SettingsShow settings)
+        {
+            //Инициализация нового ЗЕЛЕНОГО маркера, с указанием его координат
+            GMarkerGoogle marker = new GMarkerGoogle
+                (
+                 new PointLatLng(PointCurEx.Coordinates.Latitude, PointCurEx.Coordinates.Longitude),
+                 GMarkerGoogleType.green
+                 );
+            marker.ToolTip = new GMapRoundedToolTip(marker);
+
+            //Текст отображаемый при наведении на маркер
+            marker.ToolTipText = BuildToolTripText(PointCurEx, settings);
+            //Добавляем маркеры в список маркеров
+            _markersOverlay.Markers.Add(marker);
+        }
+
+        public void ShowListCurrencyExchange(List<PointCurrencyExchange> LstPointCurEx, SettingsShow settings)
+        {
+            Clear();
+            if (LstPointCurEx != null)
+            {
+                foreach (var item in LstPointCurEx)
+                {
+                    ShowCurrencyExchange(item, settings);
+                }
+            }
+        }
+    }
+}
---
+++ VISUAL_SYSTEM_OF_SEARCH_OF_OBJECTS/WorkWithXML/WorkerXML.cs
@@ -0,0 +1,168 @@
+﻿using CurrencyExchangeDAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace WorkWithXML
+{
+    public class WorkerXML
+    {
+        private string _fileName;
+        public WorkerXML()
+        {
+            _fileName = @"Data\banks.xml";
+        }
+        public List<Bank> GetBanks()
+        {
+            List<Bank> lstBanksOut = new List<Bank>();
+            NumberFormatInfo formatSepar = new NumberFormatInfo();
+            formatSepar.NumberDecimalSeparator = ".";
+            try
+            {
+                XDocument xmlDoc = XDocument.Load("http://www.obmennik.by/xml/kurs.xml");
+                XElement root = xmlDoc.Root;
+                var bankCurses = root.Elements("bank-id").
+                    Select(el =>
+                    new
+                    {
+                        BankId = int.Parse(el.Element("idbank").Value),
+                        CurrExInfo =
+                        new CurrencyExchangeInfo()
+                        {
+                            Date = DateTime.Parse(el.Element("date").Value + " " + el.Element("time").Value),
+                            Currencys = new List<Currency>()
+                            {
+                                new Currency()
+                                {
+                                    Name = "USD",
+                                    Buy = double.Parse(el.Element("usd").Element("buy").Value, formatSepar),
+                                    Sell = double.Parse(el.Element("usd").Element("sell").Value, formatSepar)
+                                },
+                                new Currency()
+                                {
+                                    Name = "EUR",
+                                    Buy = double.Parse(el.Element("eur").Element("buy").Value, formatSepar),
+                                    Sell = double.Parse(el.Element("eur").Element("sell").Value, formatSepar)
+                                },
+                                new Currency()
+                                {
+                                    Name = "RUR",
+                                    Buy = double.Parse(el.Element("rur").Element("buy").Value, formatSepar),
+                                    Sell = double.Parse(el.Element("rur").Element("sell").Value, formatSepar)
+                                }
+                            }
+                        }
+                    }
+                    );
+
+                XDocument xmlDocId = XDocument.Load(@"Data\IdBanks.xml");
+                XElement rootId = xmlDocId.Root;
+                var bankNames = rootId.Elements("bank").Select(el =>
+                    new
+                    {
+                        id = int.Parse(el.Element("id").Value),
+                        Name = el.Element("name").Value
+                    });
+                var BankCurrWithName = bankCurses.Join(bankNames, f => f.BankId, s => s.id,
+                    (f, s) => new Bank
+                    {
+                        Id = f.BankId,
+                        Name = s.Name,
+                        CurrencyExchangeInfo = f.CurrExInfo
+                    });
+                lstBanksOut = BankCurrWithName.ToList();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return lstBanksOut;
+        }
+
+        private List<PointCurrencyExchange> SetCurses(List<PointCurrencyExchange> LstPCE)
+        {
+            List<Bank> lstBanks = GetBanks();
+            foreach (var pce in LstPCE)
+            {
+                foreach (var bank in lstBanks)
+                {
+                    if (pce.Bank.Name.Equals(bank.Name))
+                    {
+                        pce.Bank = bank;
+                    }
+                }
+            }
+            return LstPCE;
+        }
+
+        public List<PointCurrencyExchange> GetPointsCurrencyExchange()
+        {
+            List<PointCurrencyExchange> lstOut = new List<PointCurrencyExchange>();
+            // установка разделитя в дробных суммах (зависит от настройки локализации)
+            NumberFormatInfo formatSepar = new NumberFormatInfo();
+            formatSepar.NumberDecimalSeparator = ",";
+            try
+            {
+                // Загрузка документа в память
+                XDocument xmlDoc = XDocument.Load(_fileName);
+                XElement root = xmlDoc.Root;
+                var banksCollext = root.Elements("bank").
+                    Select(el => new
+                    {
+                        Name = el.Attribute("name").Value,
+                        Departmen = el.Elements("departmen").Select(del => new
+                        {
+                            id = int.Parse(del.Element("id").Value),
+                            adress = del.Element("address").Value,
+                            latitude = double.Parse(del.Element("latitude").Value, formatSepar),
+                            longitude = double.Parse(del.Element("longitude").Value, formatSepar)
+                        })
+                    });
+                foreach (var bank in banksCollext)
+                {
+                    foreach (var dep in bank.Departmen)
+                    {
+                        PointCurrencyExchange PCE = new PointCurrencyExchange();
+                        PCE.Address = dep.adress;
+                        PCE.Coordinates = new Сoordinate() {Latitude = dep.latitude, Longitude = dep.longitude };
+                        var bankadd = new Bank() { Name = bank.Name };
+                        bankadd.AddPointCurrEx(PCE);
+                        PCE.Bank = bankadd;
+                        lstOut.Add(PCE);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return SetCurses(lstOut);
+        }
+
+        public List<string> GetBankName()
+        {
+            List<string> listOut = new List<string>();
+            try
+            {
+                XDocument xmlDoc = XDocument.Load(_fileName);
+                XElement root = xmlDoc.Root;
+                var banks = root.Elements("bank").
+                    Select(el => el.Attribute("name").Value);
+                listOut = banks.ToList();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return listOut;
+        }
+    }
+}
